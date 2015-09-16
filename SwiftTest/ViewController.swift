0f72a784ec20a9ff2343b6f0521c4ab57d08f419
RaymondLed <ivpmanila@IVP-MACBOOK02.local>
2015-09-16 18:59:53 +0800

    Sampe
    samplr

--- SwiftTest/ViewController.swift
+++ SwiftTest/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nametextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
